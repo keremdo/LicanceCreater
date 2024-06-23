ff346de5ae8475813b00bd0c706ed1637e38919a
Kerem Yerebakan <keremeyrbkn@gmail.com>
2024-06-24 00:47:09 +0300

    Add project files.

---
+++ LicanceCreater/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace LicanceCreater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.txtCustomerPhone = new System.Windows.Forms.TextBox();
+            this.txtCustomerTc = new System.Windows.Forms.TextBox();
+            this.txtProductNum = new System.Windows.Forms.TextBox();
+            this.btnLicanceCreate = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtPazarYeri = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Müşteri Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Firma Adı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telefon Numarası:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tc No:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ürün Kodu";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(151, 58);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(155, 22);
+            this.txtCustomerName.TabIndex = 5;
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(151, 103);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(155, 22);
+            this.txtCompanyName.TabIndex = 6;
+            // 
+            // txtCustomerPhone
+            // 
+            this.txtCustomerPhone.Location = new System.Drawing.Point(151, 146);
+            this.txtCustomerPhone.Name = "txtCustomerPhone";
+            this.txtCustomerPhone.Size = new System.Drawing.Size(155, 22);
+            this.txtCustomerPhone.TabIndex = 7;
+            // 
+            // txtCustomerTc
+            // 
+            this.txtCustomerTc.Location = new System.Drawing.Point(151, 184);
+            this.txtCustomerTc.Name = "txtCustomerTc";
+            this.txtCustomerTc.Size = new System.Drawing.Size(155, 22);
+            this.txtCustomerTc.TabIndex = 8;
+            // 
+            // txtProductNum
+            // 
+            this.txtProductNum.Location = new System.Drawing.Point(151, 217);
+            this.txtProductNum.Name = "txtProductNum";
+            this.txtProductNum.Size = new System.Drawing.Size(155, 22);
+            this.txtProductNum.TabIndex = 9;
+            // 
+            // btnLicanceCreate
+            // 
+            this.btnLicanceCreate.Location = new System.Drawing.Point(151, 290);
+            this.btnLicanceCreate.Name = "btnLicanceCreate";
+            this.btnLicanceCreate.Size = new System.Drawing.Size(155, 37);
+            this.btnLicanceCreate.TabIndex = 10;
+            this.btnLicanceCreate.Text = "Lisans Oluştur";
+            this.btnLicanceCreate.UseVisualStyleBackColor = true;
+            this.btnLicanceCreate.Click += new System.EventHandler(this.btnLicanceCreate_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(386, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(388, 256);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtPazarYeri
+            // 
+            this.txtPazarYeri.Location = new System.Drawing.Point(152, 253);
+            this.txtPazarYeri.Name = "txtPazarYeri";
+            this.txtPazarYeri.Size = new System.Drawing.Size(155, 22);
+            this.txtPazarYeri.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Pazar Yeri:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPazarYeri);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnLicanceCreate);
+            this.Controls.Add(this.txtProductNum);
+            this.Controls.Add(this.txtCustomerTc);
+            this.Controls.Add(this.txtCustomerPhone);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.TextBox txtCustomerPhone;
+        private System.Windows.Forms.TextBox txtCustomerTc;
+        private System.Windows.Forms.TextBox txtProductNum;
+        private System.Windows.Forms.Button btnLicanceCreate;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtPazarYeri;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ LicanceCreater/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace LicanceCreater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnLicanceCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Xmals adlı klasörün yolu
+                string xmalsFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Xmals");
+
+                // Eğer Xmals klasörü yoksa oluştur
+                if (!Directory.Exists(xmalsFolderPath))
+                {
+                    Directory.CreateDirectory(xmalsFolderPath);
+                }
+
+                // Oluşturulacak XML dosyasının adı ve yolu (Xmals klasöründe)
+                string xmlFileName = txtCustomerName.Text + " " + "Lisans.xml";
+                string xmlFilePath = Path.Combine(xmalsFolderPath, xmlFileName);
+
+                // XML dosyasını oluştur
+                XmlTextWriter xmlWriter = new XmlTextWriter(xmlFilePath, null);
+                xmlWriter.Formatting = Formatting.Indented;
+
+                // XML dosyasının kök elementini oluştur
+                xmlWriter.WriteStartDocument();
+                xmlWriter.WriteStartElement("Lisans");
+
+                string genericKey = GenerateLicenseKey(32);
+
+                // Müşteri bilgileri için alt elementler oluştur
+                xmlWriter.WriteStartElement("Musteri");
+                xmlWriter.WriteElementString("AdSoyad", txtCustomerName.Text);
+                xmlWriter.WriteElementString("UrunKodu", txtProductNum.Text);
+                xmlWriter.WriteElementString("TelefonNo", txtCustomerPhone.Text);
+                xmlWriter.WriteElementString("LisansAnahtari", genericKey);
+                xmlWriter.WriteElementString("LisansBaşlamaTarihi", DateTime.Now.ToString());
+                xmlWriter.WriteElementString("PazarYeri", txtPazarYeri.Text);
+                xmlWriter.WriteEndElement(); // Musteri elementi
+
+                // Kök elementi kapat
+                xmlWriter.WriteEndElement(); // Lisans elementi
+                xmlWriter.WriteEndDocument();
+
+                // XML dosyasını kapat
+                xmlWriter.Close();
+
+                MessageBox.Show("XML dosyası başarıyla oluşturuldu.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("XML dosyası oluşturulurken bir hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private string GenerateLicenseKey(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-";
+
+            // Rastgele anahtar oluştur
+            Random random = new Random();
+            char[] key = new char[length];
+            for (int i = 0; i < length; i++)
+            {
+                key[i] = chars[random.Next(chars.Length)];
+            }
+
+            return new string(key);
+        }
+    }
+}
